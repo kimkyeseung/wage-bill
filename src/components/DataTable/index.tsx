0d377103c343bf3760071ff11f0916dff48ebc8d
Kim Kyeseung <chickenboys@naver.com>
2024-10-25 00:38:31 +0900

    style: 테이블이 길어졌을 때 Form에 가리지 않도록 수정

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -11,40 +11,42 @@ export async function DataTable() {
   const paymentTotal = sumBy(data, 'payment');
 
   return (
-    <table className="min-h-[200px] w-full table-auto mb-[200px]">
-      <thead>
-        <tr className="bg-gray-2 text-left dark:bg-meta-4">
-          <Th>날짜</Th>
-          <Th>작업 장소</Th>
-          <Th>작업 내용</Th>
-          <Th>업체</Th>
-          <Th>업체 전화 번호</Th>
-          <Th>작업자</Th>
-          <Th>작업자 전화 번호</Th>
-          <Th>일당</Th>
-        </tr>
-      </thead>
-
-      <tbody className="overflow-auto">
-        {data.length ? (
-          data.map((datum) => <Row key={datum._id} datum={datum} />)
-        ) : (
-          <tr>
-            <td colSpan={10000} className="py-20 text-center">
-              No results
-            </td>
+    <div>
+      <table className="w-full table-auto mb-[200px]">
+        <thead>
+          <tr className="bg-gray-2 text-left dark:bg-meta-4">
+            <Th>날짜</Th>
+            <Th>작업 장소</Th>
+            <Th>작업 내용</Th>
+            <Th>업체</Th>
+            <Th>업체 전화 번호</Th>
+            <Th>작업자</Th>
+            <Th>작업자 전화 번호</Th>
+            <Th>일당</Th>
           </tr>
-        )}
+        </thead>
 
-        <tr>
-          <Td colSpan={10000} className="text-right border-none">
-            <strong>일당 합계:</strong>
-            <span className="ml-4 inline-block min-w-24">
-              {numberFormat(paymentTotal)} 원
-            </span>
-          </Td>
-        </tr>
-      </tbody>
-    </table>
+        <tbody className="overflow-auto pb-[200px]">
+          {data.length ? (
+            data.map((datum) => <Row key={datum._id} datum={datum} />)
+          ) : (
+            <tr>
+              <td colSpan={10000} className="py-20 text-center">
+                No results
+              </td>
+            </tr>
+          )}
+
+          <tr>
+            <Td colSpan={10000} className="text-right border-none">
+              <strong>일당 합계:</strong>
+              <span className="ml-4 inline-block min-w-24">
+                {numberFormat(paymentTotal)} 원
+              </span>
+            </Td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
