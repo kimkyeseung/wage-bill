4e44dc507e5ec68a48372c49f92d43145a52c074
Kim Kyeseung <chickenboys@naver.com>
2024-10-26 12:34:22 +0900

    chore: 운영환경/개발환경 db 분리

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME;
 
 if (!MONGODB_URI) {
   throw new Error(
@@ -12,7 +13,7 @@ async function connectToDatabase() {
   return mongoose
     .connect(MONGODB_URI, {
       autoIndex: true,
-      dbName: '노임청구서',
+      dbName: MONGODB_DB_NAME,
     })
     .then((mongoose) => {
       return mongoose.connection;
